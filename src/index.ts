dc43943c7cca2d29e62fa51c3580a1af588e3e94
Tom Stapleton <tstapleton@crunchbase.com>
2023-02-20 14:22:03 -0700

    Add pirate quote functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export default function getQuote(): string {
-	return 'Hello, world!';
+const defaultQuote = `I be cuttin' off yer arse an' feedin' it to me matey.`;
+
+export async function getQuote(): Promise<string> {
+	try {
+		const response = await fetch('http://gangstaname.com/quotes/pirate');
+		const body = await response.text();
+		const regex = /<h2[\s\w='"]*>(.*)<\/h2>/;
+		const data = regex.exec(body);
+		if (!data || !data[1]) {
+			return defaultQuote;
+		}
+		return data[1];
+	} catch (e) {
+		// can ignore any errors getting the quote
+		return defaultQuote;
+	}
 }
